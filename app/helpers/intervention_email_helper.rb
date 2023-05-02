232255c7df5ca672d6da6067832939a26ff266f0
scorpio-hank <dfantin@deakin.edu.au>
2023-05-02 20:18:50 +1000

    feat: insight engine
    + added integration for mailer (not yet connected to insight code)

--- app/helpers/intervention_email_helper.rb
+++ app/helpers/intervention_email_helper.rb
@@ -1,12 +1,22 @@
 module InterventionEmailHelper
+  def markdown_to_html(text)
+    options = {
+      filter_html: true,
+      hard_wrap: true,
+      link_attributes: { rel: 'nofollow', target: '_blank' },
+      space_after_headers: true,
+      fenced_code_blocks: true
+    }
 
-  def markdown(text)
-      extensions = %i[
-          hard_wrap autolink no_intra_emphasis tables fenced_code_blocks
-          disable_indented_code_blocks strikethrough lax_spacing space_after_headers
-          quote footnotes highlight underline
-          ]
-      Markdown.new(text, *extensions).to_html.html_safe
+    extensions = {
+      autolink: true,
+      superscript: true,
+      disable_indented_code_blocks: true
+    }
+
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
   end
-
 end
--- app/mailers/intervention_action_mailer.rb
+++ app/mailers/intervention_action_mailer.rb
@@ -1,20 +1,24 @@
 class InterventionActionMailer < ActionMailer::Base
+  helper :intervention_email
+  default from: 'no-reply@example.com'
+
   def add_general
-      @doubtfire_host = Doubtfire::Application.config.institution[:host]
-      @doubtfire_product_name = Doubtfire::Application.config.institution[:product_name]
-      # Removed unsubscribe option
-    end
+    @doubtfire_host = Doubtfire::Application.config.institution[:host]
+    @doubtfire_product_name = Doubtfire::Application.config.institution[:product_name]
+    # Removed unsubscribe option
 
-  def email_action(project, email_subject, email_header, email_template)
-      add_general
+    # TODO: Amend the FROM to change to tutor's email"
+  end
 
-      @student = project.student
-      @project = project
-      @email_header = email_header
-      @email_template = email_template
+  def intervention_action_email(project, email_subject, email_header, email_template)
+    add_general
 
-      email_with_name = %("#{@student.name}" <#{@student.email}>)
-      # TODO: Get the user's email address(?)
-      mail(to: email_with_name, from: tutor_email, subject: email_subject)
+    @student = project.student
+    @project = project
+    @email_header = email_header
+    @email_template = email_template
+    email_with_name = %("#{@student.name}" <#{@student.email}>)
+
+    mail(to: email_with_name, subject: email_subject)
   end
 end
